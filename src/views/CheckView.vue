a8c3ec64553c6db3e63bb038eac3b2f34b35ff69
travelhalyk <admin@halyktravel.kz>
2023-09-24 01:25:26 +0600

    feat: save commit

--- src/views/CheckView.vue
+++ src/views/CheckView.vue
@@ -1,44 +1,51 @@
 <script setup>
 
-</script>   
+</script>
 
 <template>
-    <div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, officiis? Asperiores, suscipit exercitationem modi vel nisi molestiae sequi eligendi enim nulla iusto ratione qui harum quasi sed laborum omnis voluptates?</p>
-    </div>
-
+  <MDBCard>
+    <MDBCardImg
+        src="https://mdbootstrap.com/img/new/standard/nature/184.webp"
+        top
+    />
+    <MDBCardBody  class="mt-5 mb-5">
+      <MDBCardTitle>Card title</MDBCardTitle>
+      <MDBCardText>
+        Some quick example text to build on the card title and make up the bulk of the
+        card's content.
+      </MDBCardText>
+    </MDBCardBody>
+  </MDBCard>
+  <MDBCard>
+    <MDBCardBody>
+      <MDBCardText>
+        Some quick example text to build on the card title and make up the bulk of the
+        card's content.
+      </MDBCardText>
+    </MDBCardBody>
+  </MDBCard>
 </template>
-  
-<style>
-p{
-    margin: 0;
-    padding: 0;
-}
-</style>
+
 
 <script>
 
-import { ref } from 'vue';
+import {ref} from 'vue';
+import {MDBBtn, MDBCard, MDBCardBody, MDBCardImg, MDBCardText, MDBCardTitle} from "mdb-vue-ui-kit";
 
 
 export default {
-    methods: {
-        goBack() {
-            this.$router.go(-1)
-        }
+  components: {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImg,
+    MDBBtn
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1)
     }
+  }
 }
 </script>
